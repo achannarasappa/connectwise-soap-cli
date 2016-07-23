e4c8d50905dbc8fde03930182dab0a6a80e6c5ba
jacobbridges <him@jacobandkate143.com>
2016-07-23 17:42:47 -0500

    changed execfile for loading app metadata to an abs path

--- cwcli/__init__.py
+++ cwcli/__init__.py
@@ -3,7 +3,11 @@ ccli/
 
 This module contains the source code for the ConnectWise SOAP CLI.
 """
-execfile('./meta.py')
+import os
+
+
+# Load the application metadata
+execfile(os.path.dirname(os.path.realpath(__file__)) + '/meta.py')
 
 ####################################################################################################
 # Module Definitions
