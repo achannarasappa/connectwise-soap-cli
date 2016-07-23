cd457b53c0ccec5af1b9d018ae9abf58d5ade44c
jacobbridges <him@jacobandkate143.com>
2016-07-23 17:29:10 -0500

    increased version

--- cwcli/meta.py
+++ cwcli/meta.py
@@ -7,4 +7,4 @@ Metadata about the application.
 __app__ = 'cwcli'
 __description__ = 'Command line interface for the ConnectWise SOAP API.'
 __author__ = 'Jacob A. Bridges'
-__version__ = '0.0a1'
+__version__ = '0.0a2'
